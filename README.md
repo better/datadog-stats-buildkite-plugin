5f6e9e1144d216c81e640da839e4971bd0fb5cd9
Ethan Langevin <ethan@better.com>
2020-05-12 23:17:47 -0400

    Support specifying additional values either hardcoded or pulling from an env var

--- README.md
+++ README.md
@@ -21,6 +21,22 @@ steps:
 The host where this agent can reach a running instance of dogstatsd for
 reporting metrics to.
 
+### `dogstatsd_port` (Optional, string)
+
+The port where this agent can reach dogstatsd on wherever is specified
+in `dogstatsd_host`. This defaults to the default port for dogstatsd
+which is `8125`.
+
+### `additional_tags` (Optional, array)
+
+This is an array of additional tags you want to send and where to find
+them in the environment variables. Each entry is an object with
+properties
+
+* `tag` which is the tag name to send to datadog
+* `env_var` which is the environment variable to pull the tag's value from
+* `value` which is a static value to send for that tag
+
 ## Developing
 
 To run the tests:
--- lib/get-tags.sh
+++ lib/get-tags.sh
@@ -1,3 +1,29 @@
+#!/bin/bash
+
+function readAdditionalTags() {
+  local result=()
+  # Provide a limit to avoid accidental infinite loops
+  for i in {0..100}; do
+    tag_var="BUILDKITE_PLUGIN_DATADOG_STATS_ADDITIONAL_TAGS_${i}_TAG"
+    env_var="BUILDKITE_PLUGIN_DATADOG_STATS_ADDITIONAL_TAGS_${i}_ENV_VAR"
+    value_var="BUILDKITE_PLUGIN_DATADOG_STATS_ADDITIONAL_TAGS_${i}_VALUE"
+
+    if [[ -z ${!tag_var:-} ]]; then
+      break;
+    fi
+
+    if [ -z ${!env_var:-} ]; then
+      # If env_var is not set, then a hard-coded value must be
+      result+=("${!tag_var}:${!value_var}")
+    else
+      local env_var_value=${!env_var}
+      result+=("${!tag_var}:${!env_var_value:-}")
+    fi
+  done
+
+  echo -n "$(IFS=$','; echo "${result[*]}" | sed 's/,$//')"
+}
+
 function getTags() {
   BK_COMMAND=${BUILDKITE_COMMAND}
   BK_LABEL=${BUILDKITE_LABEL}
@@ -10,6 +36,13 @@ function getTags() {
 
   PIPELINE_SLUG=${BUILDKITE_PIPELINE_SLUG}
   RETRY_COUNT=${BUILDKITE_RETRY_COUNT:-0}
+  ADDITIONAL_TAGS=$(readAdditionalTags)
 
-  echo is_master:${IS_MASTER},pipeline_slug:${PIPELINE_SLUG},step_command:${BK_COMMAND},step_label:${BK_LABEL},retry_count:${RETRY_COUNT}
+  RESULT="is_master:${IS_MASTER},pipeline_slug:${PIPELINE_SLUG},step_command:${BK_COMMAND},step_label:${BK_LABEL},retry_count:${RETRY_COUNT}"
+
+  if [ -n "ADDITIONAL_TAGS" ]; then
+    RESULT+=",${ADDITIONAL_TAGS}"
+  fi
+
+  echo "${RESULT}"
 }
