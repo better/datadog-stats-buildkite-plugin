b4183ab6904e6373892d9921472f4b06d9cfc899
Ben Connito <bconnito@better.com>
2020-11-05 21:09:09 -0500

    re-enable metrics

--- lib/report-to-datadog.sh
+++ lib/report-to-datadog.sh
@@ -11,5 +11,5 @@ function reportToDatadog() {
 
   echo "Reporting ${metric_name} with value=${metric_value}, type=${metric_type}, tags=${tags}"
   echo "Sending to ${DD_HOST}:${DD_PORT}..."
-#  echo -n "${metric_name}:${metric_value}|${metric_type}|#${tags}" | docker run -i --rm artifactory.bettermg.com/docker/subfuzion/netcat -4u -w1 "${DD_HOST}" "${DD_PORT}"
+  echo -n "${metric_name}:${metric_value}|${metric_type}|#${tags}" | docker run -i --rm artifactory.bettermg.com/docker/subfuzion/netcat -4u -w1 "${DD_HOST}" "${DD_PORT}"
 }
